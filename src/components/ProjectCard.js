d2faa0d5c4cf8a17c6b83237da665c3879fbc127
aecel <aecelacuna@gmail.com>
2022-10-20 20:28:23 +0800

    Add react

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,15 @@
+const ProjectCard = () => {
+  return (
+    <a
+      target="_blank"
+      rel="noreferrer"
+      id="rock-paper-scissors"
+      href="https://aecel.github.io/rock-paper-scissors/"
+      class="card"
+    >
+      <img src="./images/rock-paper-scissors.png" alt="" />
+    </a>
+  )
+}
+
+export default ProjectCard
