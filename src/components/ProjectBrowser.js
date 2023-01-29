c3961a22a7bd34c02b88f4b99e3ebc8b58aaa4ff
aecel <aecelacuna@gmail.com>
2023-01-29 10:44:08 +0800

    Make tab switching work in the ProjectBrowser, add sample projects

--- src/components/ProjectBrowser.js
+++ src/components/ProjectBrowser.js
@@ -1,6 +1,25 @@
-import habbitAppGif from "../images/projects/shopping-cart.gif"
+import { useState } from "react"
+import getOdinProjects from "../getOdinProjects"
+import habbitAppGif from "../images/projects/habit-app.gif"
 
 const ProjectBrowser = () => {
+  const projectArray = getOdinProjects()
+  const [chosenProject, setChosenProject] = useState(projectArray[0])
+
+  const getIndexById = (id) => {
+    for (const project of projectArray) {
+      if (project.id === id) {
+        return projectArray.indexOf(project)
+      }
+    }
+
+    return null
+  }
+  const chooseTab = (event) => {
+    console.log(event.target.dataset.key)
+    const index = getIndexById(event.target.dataset.key)
+    setChosenProject(projectArray[index])
+  }
   return (
     <div className="project-browser-container">
       <div className="project-browser-header">
@@ -8,22 +27,37 @@ const ProjectBrowser = () => {
         <div className="project-browser-header-button"></div>
         <div className="project-browser-header-button"></div>
       </div>
-      <div className="project-navbar">
-        <div className="project-navbar-tab"></div>
-        <div className="project-navbar-tab"></div>
-        <div className="project-navbar-tab"></div>
-        <div className="project-navbar-tab"></div>
-        <div className="project-navbar-tab"></div>
+      <div
+        className="project-navbar"
+        style={{
+          gridTemplateColumns: `repeat(1fr, ${projectArray.length})`,
+        }}
+      >
+        {projectArray.map((project) => {
+          return (
+            <div
+              data-key={project.id}
+              key={project.id}
+              onClick={chooseTab}
+              className={
+                project.id === chosenProject.id
+                  ? "chosen-project project-navbar-tab"
+                  : "project-navbar-tab"
+              }
+            >
+              {project.name}
+            </div>
+          )
+        })}
       </div>
       <div className="project-browser">
         <a
           target="_blank"
           rel="noreferrer"
           className="project-gif"
-          id="ID"
-          href="{link}"
+          href={chosenProject.githubLink}
         >
-          <img src={habbitAppGif} alt="{alt}" />
+          <img src={chosenProject.imageSrc} alt={chosenProject.alt} />
         </a>
       </div>
       <div className="project-info"></div>
--- src/getOdinProjects.js
+++ src/getOdinProjects.js
@@ -1,24 +1,53 @@
 import shoppingCartGif from "./images/projects/shopping-cart.gif"
 import habbitAppGif from "./images/projects/habit-app.gif"
+import cvProjectGif from "./images/projects/cv-project.gif"
+import memoryGameGif from "./images/projects/memory-game.gif"
+import weatherAppGif from "./images/projects/weather-app.gif"
+
 
 const getOdinProjects = () => {
   const odinProjectsArray = [
     {
       id: "habit-app",
-      name: "Merciful Habit Forming App",
+      name: "Habit Forming App",
       imageSrc: habbitAppGif,
       link: "https://aecel.github.io/habit-app/",
       githubLink: `https://github.com/aecel/habit-app`,
-      alt: "Link to Merciful Habit Forming App",
+      alt: "Link to Habit Forming App",
     },
-    // {
-    //   id: "shopping-cart",
-    //   name: "Shopping Cart",
-    //   imageSrc: shoppingCartGif,
-    //   link: "https://aecel.github.io/shopping-cart/",
-    //   githubLink: `https://github.com/aecel/shopping-cart`,
-    //   alt: "Link to Shopping Cart",
-    // },
+    {
+      id: "shopping-cart",
+      name: "Shopping Cart",
+      imageSrc: shoppingCartGif,
+      link: "https://aecel.github.io/shopping-cart/",
+      githubLink: `https://github.com/aecel/shopping-cart`,
+      alt: "Link to Shopping Cart",
+    },
+    {
+      id: "cv-project",
+      name: "CV Generator",
+      imageSrc: cvProjectGif,
+      link: "https://aecel.github.io/cv-project/",
+      githubLink: `https://github.com/aecel/cv-project`,
+      alt: "Link to CV Generator",
+    },
+    {
+      id: "memory-game",
+      name: "Memory Game",
+      imageSrc: memoryGameGif,
+      link: "https://aecel.github.io/memory-game/",
+      githubLink: `https://github.com/aecel/memory-game`,
+      alt: "Link to Memory Game",
+    },
+    {
+      id: "weather-app",
+      name: "Weather App",
+      imageSrc: weatherAppGif,
+      link: "https://aecel.github.io/weather-app/",
+      githubLink: `https://github.com/aecel/weather-app`,
+      alt: "Link to Weather App",
+    },
+
   ]
 
   return odinProjectsArray
