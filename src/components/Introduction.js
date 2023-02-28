0ab2e15eafd4db775021981b9a173f86621ea5e5
aecel <aecelacuna@gmail.com>
2023-02-28 20:50:30 +0800

    Change Introduction padding / styling

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,7 +1,20 @@
+import introductionGif from "../images/Introduction.gif"
 const Introduction = () => {
   return (
     <div id="Introduction">
       <div className="main-with-max-width">
+        {/* <img
+          src={introductionGif}
+          alt=""
+          style={{
+            width: "300px",
+            height: "300px",
+            // border: "white 2px dashed",
+            borderRadius: "50%",
+            objectPosition: "bottom",
+            // objectFit: "contain",
+          }}
+        /> */}
         <h2>Hi, I'm Aecel</h2>
         <div>I'm a front-end web developer</div>
       </div>
